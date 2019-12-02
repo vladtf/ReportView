303986f6e72e5fc54e360a1b07f61dcb1bfb57b1
Vlad Tiftilov <vendeta0263@gmail.com>
2019-12-02 20:36:17 +0200

    Added basic tables for workers and departments.

---
+++ VTFData/dbo/Tables/Departments.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Departments] (
+    [Department]    NVARCHAR (50) NOT NULL,
+    [City]          NVARCHAR (50) NOT NULL,
+    [Phone_Number]  NVARCHAR (50) NOT NULL,
+    [Street_Adress] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Department] ASC)
+);
+
