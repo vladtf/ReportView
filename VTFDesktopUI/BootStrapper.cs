289fa39cf1e5f9370b68888ecfa5970d7a5281e2
Vlad Tiftilov <vendeta0263@gmail.com>
2019-12-02 22:23:12 +0200

    Added a interface to login + button firing

--- VTFDesktopUI/BootStrapper.cs
+++ VTFDesktopUI/BootStrapper.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using VTFDesktopUI.Helpers;
 using VTFDesktopUI.ViewModels;
 
 namespace VTFDesktopUI
@@ -15,6 +17,11 @@ namespace VTFDesktopUI
         public BootStrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
 
         protected override void Configure()
--- VTFDesktopUI/ViewModels/ShellViewModel.cs
+++ VTFDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace VTFDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<Object>
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+
+            ActivateItem(_loginVM);
+        }
     }
 }
